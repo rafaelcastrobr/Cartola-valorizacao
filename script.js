176549c8cc38b83a14a2b73b325a9bc10fb86d98
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-06-09 14:08:30 -0300

    Update correct js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 
-function resultA() {
 
+
+function resultA() {
+  var coeficientePt = parseFloat(document.getElementById("coeficiente-pt").value);
   var prJogador = parseFloat(document.getElementById("pr-jogador").value);
   var ptRodadaDois = parseFloat(document.getElementById("pt-rodada-dois").value);
-  var ptRodadaUm = parseFloat(document.getElementById("pt-rodada-um").value);
+  var ptRodadaUm = parseFloat(document.getElementById("pt-rodada-um").value); 
 
-  var resultadoA = ((4.9 * prJogador + ptRodadaDois) - 2 * ptRodadaUm) / 14;
+  var resultadoA = ((coeficientePt * prJogador + ptRodadaDois) - 2 * ptRodadaUm) / 14;
 
   document.getElementById("resultado-final").innerHTML = resultadoA.toFixed(2);
+
+  console.log(coeficientePt);
 
 }
 
 
 
 function result() {
-
+  var coeficientePt = parseFloat(document.getElementById("coeficiente-pt-a").value);
   var prJogador = parseFloat(document.getElementById("pr-jogador-b").value);
   var ptRodadaUm = parseFloat(document.getElementById("pt-rodada-um-b").value);
 
-  var resultadoA = ((4.9 * prJogador)- 4 * ptRodadaUm) / 7;
+  var resultadoA = ((coeficientePt * prJogador)- 4 * ptRodadaUm) / 7;
 
   document.getElementById("resultado-final-b").innerHTML = resultadoA.toFixed(2);
 
+  console.log(coeficientePt);
 }
