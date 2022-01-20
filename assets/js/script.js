8378a46d35c497898bbcd7ebbffc31af3c0cb48d
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-01-20 09:31:09 -0300

    fix url json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 import { $result, $input } from './module/$local.js'
 
-//let requestURL = 'https://raw.githubusercontent.com/rafaelcastrobr/Cartola-valorizacao/newCartola/assets/json/rodada2.json';
-let requestURL = '../assets/json/rodada2.json'
+let requestURL = 'https://raw.githubusercontent.com/rafaelcastrobr/Cartola-valorizacao/newCartola/assets/json/rodada2.json';
+//let requestURL = '../../Cartola-Valorizacao/assets/json/rodada2.json'
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 
